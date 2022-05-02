4dfa35c5b5a28f45f78d3455b0b5d39a87550da8
MD Sazid <mdsaziduli14@gmail.com>
2022-05-03 02:29:41 +0600

    my item list route done

--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useData from '../../Hooks/useData';
+import MyItem from './MyItem';
 
 const MyItems = () => {
+    const [items] = useData()
     return (
         <div>
-            
+            <div className='row row-cols-1 row-cols-md-3 g-4 container mx-auto'>
+                {
+                    items.map(item => <MyItem key={item._id} item={item}></MyItem>)
+                }
+        </div>
         </div>
     );
 };
