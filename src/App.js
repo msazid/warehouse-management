67449e4265894373de3bebbd1679d5a737da383f
MD Sazid <mdsaziduli14@gmail.com>
2022-05-02 22:27:18 +0600

    add some pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Header from './Pages/Shared/Header/Header';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import MyItems from './Pages/MyItems/MyItems';
+import AddItem from './Pages/AddItem/AddItem';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-       <h1>Yo</h1>
+       <Header></Header>
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/blog' element={<Blogs/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/myitems' element={<MyItems/>}/>
+          <Route path='/additem' element={<AddItem/>}/>
+          <Route path='*' element={<NotFound/>}/>
+       </Routes>
     </div>
   );
 }
---
+++ src/Hooks/CustomHook.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const [item,setItem] = useState()
+
+useEffect(() => {
+    
+}, []);
