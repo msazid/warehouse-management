897d4dda93ced819537b1e0954de4489eaeb5717
MD Sazid <mdsaziduli14@gmail.com>
2022-05-05 18:45:27 +0600

    login part started

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div>
             <div className='hero-section d-flex align-items-center justify-content-center'>
                 <div className='container'>
-                    <h1 className='banner-title text-light'>Welcome to <span className="text-color">MS Grocery</span> Warehouse</h1>
+                    <h1 className='banner-title text-center text-light'>Welcome to <span className="text-color">MS Grocery</span> Warehouse</h1>
                     <p className='banner-text text-light'>We'll never walk alone.You can easily update or mange the item list. <br /> Facing any problem feel free to give us any king of feedback.We'll  <br />always there for you and want to hare from you</p>
                     <span className='d-flex justify-content-center'>
                         <Link className='btn-banner me-2' to='/manageitems'>Manage Item</Link>
@@ -18,7 +18,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="container my-3">
-                <h1 >Product List </h1>
+                <h1 className='text-center'>Product List </h1>
             </div>
             <Items></Items>
         </div>
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import deliveryPic from '../../../Assets/Image/delivery.jpg'
 const Login = () => {
     return (
         <div>
-            
+            <div className="row container w-75 border mx-auto">
+                <div className="col-12 col-md-6 border">
+                    <img className='img-fluid' src={deliveryPic} alt="" />
+                </div>
+                <div className="col-12 col-md-6">
+                    <form className='border border-2'>
+                        <div class="mb-3">
+                            <span className="text-start">Email</span>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                            <span className='text-start'>Password</span>
+                            <input type="password" class="form-control" id="exampleInputPassword1" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
